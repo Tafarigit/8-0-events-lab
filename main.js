cf40547651d74838c78ff5b6416177b41c2976b3
Tafari Excell <excell.tafari@gmail.com>
2022-12-19 09:26:05 -0500

    All test passing including additionals

--- main.js
+++ main.js
@@ -1,9 +1,42 @@
 // Do not change the code below.
 const main = document.querySelector("main");
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < 200; i++) {
   const div = document.createElement("div");
   div.classList.add("cell");
   main.append(div);
 }
 
 // You may write your code here!
+// const currentColor = document.querySelector("#current-color")
+
+let colors = document.querySelectorAll(".color")
+const currentColor = document.querySelector("#current-color")
+for (let color of colors) {
+  color.addEventListener('click', (event) => {
+
+    currentColor.style.background = event.target.style.background;
+  })
+}
+
+const canvas = document.querySelector("#canvas")
+const cells = document.querySelectorAll(".cell")
+
+
+for (let cell of cells) {
+  
+  cell.addEventListener ("click", (event) => {
+   
+    cell.style.background = currentColor.style.background;
+  });
+}
+
+// canvas.addEventListener()
+
+
+for (let cell of cells) {
+  cell.addEventListener ("dragover", (event) => {
+   
+    cell.style.background = currentColor.style.background;
+  });
+}
+
